93e2d40fed75311bb8fa49f0036bf9b904b6b5b5
Adrian <villararceo@gmail.com>
2022-02-18 16:36:06 -0600

    coderhouse-Desafio_01

---
+++ coderhouse/Desafio_01/Desafio_01.js
@@ -0,0 +1,12 @@
+class Usuario{
+	constructor(nombre, apellido, libros, mascotas){
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.libros = libros;
+		this.mascotas = mascotas;
+	};
+
+	// Métodos de mi clase
+};
+
+// creación de objeto
