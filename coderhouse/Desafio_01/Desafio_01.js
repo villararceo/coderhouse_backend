83284d07678cdb9e5dd9cf93583832d467734b4a
Adrian <villararceo@gmail.com>
2022-02-19 17:10:42 -0600

    Desafio 01

--- coderhouse/Desafio_01/Desafio_01.js
+++ coderhouse/Desafio_01/Desafio_01.js
@@ -7,23 +7,64 @@ class Usuario{
 	};
 
 	// Métodos de mi clase
-	getFullName(): String
-	addMascota(string): void
-	countMascotas(): Number
-	addBook(String, String): void
-	getBookNames(): String[]
+	getFullName(nombre, apellido) {
+		//retornar nombre completo del usuario usando template string
+		return `nombre: ${nombre} - apellido: ${apellido}`;
+	};
 
+	countMascotas() {
+                //retornar la cantidad de mascotas que tiene el usuario: number
+		return this.mascotas.length;
+        }; 
+
+	addMascotas(nombreMascota){
+                //añade una mascota al array mascotas
+		this.mascotas.push(nombreMascota);
+        }; 
+
+	addBook(nombre, autor){
+                //añadir nombre y autor de libro al aaarray libros
+		// objeto literal: { clave: valor }
+		this.libros.push({ nombreLibro: nombre, nombreAutor: autor });
+        };
+
+	getBookNames(){
+                //retornar solamente los nombres de los libros que se encuentran en el array libros
+		// vamos a recorrer  el arreglo libros, acceder a las claves nombreLibro y 
+		// retornar un nuevo arreglo que contiene solo los nombres de los libros. De todos
+		// los elementos encontrados. En este caso existen solo 2
+
+		/* bucle for
+		const listaNombres = [];
+		const longitudLibros = this.libros.length;
+		const listaLibros = this.libros;
+		for(let n = 0; n < longitudLibros; n++) { 
+			listaNombres.push(listaLibros[n].nombreLibro);
+		};
+		return listaNombres; */
+
+		/* bucle for...in
+		const listaNombres = [];
+		const listaLibros = this.libros;
+		for(const x in listaLibros){
+			listaNombres.push(listaLibros[x].nombreLibro);
+		};
+		return listaNombres; */
+
+		// Estoy mapeando el arreglo this.libros
+		return this.libros.map(libro => libro.nombreLibro);
+
+	};
 };
 
 // creación de objeto
 console.log("instancia de objeto");
-let nombre = "Adrian"
-let apellido = "Villar"
-let libros = ["1984", "George Orwell"]
-let mascotas = ["gato", "perro"]
-
-Usuario.getFullName():
-addMascota(string): "gato 2"
-countMascotas():
-addBook(String, String):[{nombre: "Animal Farm", autor: "George Orwell"}]
-Usuario.getBookNames()
+const usuario = new Usuario("Adrian", "Villar", [], []);
+usuario.addMascotas("Aguacate");
+usuario.addMascotas("Chapata");
+console.log(usuario.countMascotas());
+usuario.addBook("1984", "George Orwell");
+usuario.addBook("El Señor de las Moscas", "William Golding");
+usuario.addBook("El Señor de las Moscas", "William Golding");
+console.log(usuario.getBookNames());
+console.log(usuario.getFullName("Adrian", "Villar"));
