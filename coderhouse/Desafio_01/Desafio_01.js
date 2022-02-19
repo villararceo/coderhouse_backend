04acde2bdff8abc6036dc17770aa9caff4d97fa0
Adrian <villararceo@gmail.com>
2022-02-18 18:39:48 -0600

    entrega ejercicio

--- coderhouse/Desafio_01/Desafio_01.js
+++ coderhouse/Desafio_01/Desafio_01.js
@@ -7,7 +7,23 @@ class Usuario{
 	};
 
 	// Métodos de mi clase
+	getFullName(): String
+	addMascota(string): void
+	countMascotas(): Number
+	addBook(String, String): void
+	getBookNames(): String[]
+
 };
 
 // creación de objeto
 console.log("instancia de objeto");
+let nombre = "Adrian"
+let apellido = "Villar"
+let libros = ["1984", "George Orwell"]
+let mascotas = ["gato", "perro"]
+
+Usuario.getFullName():
+addMascota(string): "gato 2"
+countMascotas():
+addBook(String, String):[{nombre: "Animal Farm", autor: "George Orwell"}]
+Usuario.getBookNames()
